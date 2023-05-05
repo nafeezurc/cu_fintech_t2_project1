36514c29bf5b856a73268294f03dd482fed277bf
nafeezurc <nafeezurc@gmail.com>
2023-05-04 19:18:12 -0500

    getting etf data

--- stocks.py
+++ stocks.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import yfinance as yf
+
+tqqq = yf.Ticker("TQQQ")
+tqqq_hist = tqqq.history()
+tqqq_closing = pd.DataFrame(tqqq_hist["Close"])
+
+
