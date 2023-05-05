c1484b46c4e3c8196d642e0fa5a1d37e703ce2a7
lumiroga <lumiroga@gmail.com>
2023-05-04 20:17:56 -0400

    Crypto to dataframe

--- crypto_alpaca.py
+++ crypto_alpaca.py
@@ -5,14 +5,30 @@ from alpaca.data.timeframe import TimeFrame
 
 
 from datetime import datetime
-
-client = CryptoHistoricalDataClient()
-request_params = CryptoBarsRequest(
-                        symbol_or_symbols=["BTC/USD", "ETH/USD"],
-                        timeframe=TimeFrame.Day,
-                        start=datetime(2018, 1, 1)
-                        )
-bars = client.get_crypto_bars(request_params)
+import pandas as pd
 
 
-print(bars)
+def get_crpto_df(from_date,to_date = datetime.now(),crypto_exchange=["BTC/USD", "ETH/USD"]):
+    """Gets live Crypto Data
+
+    Keyword arguments:
+    :param datetime from_date: initial date
+    :param dateime to_date: last date
+    :param list tickers -- currency Pairs list to retrieve
+    """
+    if from_date is None:
+        raise AttributeError("from_date is a required field")
+    
+    client = CryptoHistoricalDataClient()
+    request_params = CryptoBarsRequest(
+                            symbol_or_symbols=crypto_exchange,
+                            timeframe=TimeFrame.Day,
+                            start=from_date
+                            )
+    crypto_df = client.get_crypto_bars(request_params).df
+
+    crypto_dict ={}
+
+    print(crypto_df)
+
+get_crpto_df(datetime(2022, 7, 1))
