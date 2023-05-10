916cc8fc96fb83473afa700baa5e145a9ed8bf19
lumiroga <lumiroga@gmail.com>
2023-05-09 20:09:37 -0400

    Remove MonteCarlo output

--- MCForecastTools.py
+++ MCForecastTools.py
@@ -93,7 +93,7 @@ class MCSimulation:
         for n in range(self.nSim):
         
             if n % 10 == 0:
-                print(f"Running Monte Carlo simulation number {n}.")
+                pass#print(f"Running Monte Carlo simulation number {n}.")
         
             # Create a list of lists to contain the simulated values for each stock
             simvals = [[p] for p in last_prices]
