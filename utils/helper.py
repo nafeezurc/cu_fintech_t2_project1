2a8615f4fa87324a0f93b98e646599dc25a8d1f3
lumiroga <lumiroga@gmail.com>
2023-05-11 19:14:25 -0400

    typo

--- utils/helper.py
+++ utils/helper.py
@@ -11,8 +11,9 @@ def interpret_results(table,initial_investment,name,investment_timeframe):
     #print(type(lower_95),type(upper_95),type(initial_investment))
 
     return f"""
-          {name}, with your current investment of ${initial_investment}. You will get an  estimated return of {round(mean*100,2)}% 
-           with a total between ${round(lower_95*initial_investment,2)} in {investment_timeframe} years and 
-          ${round(upper_95*initial_investment,2)}
+          {name}, with your current investment of ${initial_investment}.
+          You will get an  estimated return of {round(mean*100,2)}%
+        with a total between ${round(lower_95*initial_investment,2)} years and 
+        ${round(upper_95*initial_investment,2)} in {investment_timeframe} years.
           """
     
